eef034dc7a80de77fda43f2bda364f5f5545e37e
Yasser Janah <contact@yasser-janah.com>
2022-01-21 12:34:36 +0100

    add project files

---
+++ routes/movies.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+router.get('/', async function(req, res, next) {
+    const take = req.query.take ? parseInt(req.query.take) : 10;
+    const skip = req.query.skip ? parseInt(req.query.skip) : 10;
+
+    const movies = await prisma.movies.findMany({
+        skip,
+        take: take,
+    });
+
+    // get paginated movies count
+    const total = movies.length;
+
+    res.status(200).send({
+        data: movies,
+        pagination: {
+            count: total, // Total des enregistrements
+            take: take, // Nombre d'éléments sélectionnés
+            skip: skip // Décalage à partir duquel on prend les  données
+        }
+    });
+
+});
+
+module.exports = router;
